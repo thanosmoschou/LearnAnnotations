27b24307520ef1985874cd3803695fb480084ff3
Thanos Moschou <130168149+thanosmoschou@users.noreply.github.com>
2023-12-14 22:52:40 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,7 @@ public class Main
 		Animal cat = new Cat("mia");
 		Animal dog = new Dog("max");
 		
-  //With getClass() method or class literal I can get the instance(object) of a class of a running program
+  //With getClass() method or class literal I can get the instance(object) of a class or an interface of a running program
   //For example with myObject.getClass() or myObject.class I get the instance of the class that myObject belongs to.
 		
 		//check if Cat has the VeryImportant annotation
